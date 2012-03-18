8803a09306113afcf6db93ad28b5cd71f172e8dd
Alex Kerr <Alex@alex-laptop>
2012-03-18 13:09:00 -0700

    Fixes in Game.play(), added simple GUI
    Fixed some placement check bugs, added starting corners, and hacked up a
    test GUI using the WxPython Tetris example here:
    http://zetcode.com/wxpython/thetetrisgame/

--- test/Board.py
+++ test/Board.py
@@ -21,38 +21,49 @@ pieces = {'5W': [[1, 0], [2, 0], [0, 1], [1, 1], [0, 2]],
           '5P': [[0, 0], [1, 0], [2, 0], [0, 1], [1, 1]],                                 
           '2I': [[0, 0], [1, 0]],                                                         
           '1O': [[0, 0]]}
-
+          
 class Game():
     def place(self, player, tiles):
         """ Expects a player number and an array of [x,y] locations for tiles """
         for tile in tiles:
             x, y = tile
             self.board[x][y] = player
+            
     def __init__(self):
         self.board = [[0 for i in range(20)] for j in range(20)]
         self.turn = 0
         self.players = { i: pieces.keys() for i in range(4) }
+        self.start_corners = [[0,0], [19,0], [19,19], [0,19]]
+        self.curr_turn = 1
+    
     def play(self, player, piece, rotations, flipped, location):
         #if (1 != self.players.get(player).count(piece)):
         #    return False
         p = translatePiece(piece, rotations, flipped, location)
         corner = False
+        
         for tile in p:
-            x = tile[0]
-            y = tile[1]
-            if (x < 0 or x > 20 or y < 0 or y > 20 or self.board[x][y] != 0):
+            x,y = tile
+            if (x < 0 or x >= len(self.board) or y < 0 or y >= len(self.board[0]) or self.board[x][y] != 0):
                 print "Overlap or out of bounds"
                 return False
             for i,j in [[-1,0],[0,1],[1,0],[0,-1]]:
-                if (self.board[x+i][y+j] == player):
+#                print x+i,y+j
+                if (x+i < 0 or x+i >= len(self.board) or y+j < 0 or y+j >= len(self.board[0])):
+                    pass
+                elif (self.board[x+i][y+j] == player):
                     print "tesselated"
                     return False
+            if x == self.start_corners[player-1][0] and y == self.start_corners[player-1][1]:
+                corner = True
             for i,j in [[-1,-1],[-1,1],[1,-1],[1,1]]:
-                if (self.board[x+i][y+j] == player):
-                    corner = True
+                if (x+i < 0 or x+i >= len(self.board) or y+j < 0 or y+j >= len(self.board[0])):
+                    pass
+                elif (self.board[x+i][y+j] == player):
+                    corner = True  
         if (not corner):
             print "No connections"
-            #return False
+            return False
         for tile in p:
             self.board[tile[0]][tile[1]] = player
         return True
@@ -92,3 +103,38 @@ def printPiece(piece):
         for j in range(len(fig[i])):
             print fig[i][j],
         print '\n',
+
+def test(game):
+#    game = Game()
+    curr_player = 1
+    while True:
+        print "Player %s" % (curr_player)
+        for i in range(20):
+            for j in range(20):
+                if game.board [j][i]:
+                    print game.board[j][i],
+                else:
+                    print '.',
+            print ''
+        key = input("Piece? ")
+        rot = input("Rotation? ")
+        flip = input("Flipped? ")
+        ix = input("X? ")
+        iy = input("Y? ")
+        if game.play(curr_player, pieces[key], rot, flip, [ix,iy]):
+            curr_player += 1
+            if(curr_player > 4):
+                curr_player = 1 
+        else:
+            print 'Invalid move!'
+        
+#    game.play(1, pieces['5W'], 1, 0, [0,0])
+#    game.play(2, pieces['5V'], 1, 0, [17,0])
+#    game.play(3, pieces['5V'], 2, 0, [17,17])
+#    game.play(4, pieces['5V'], 3, 0, [0,17])
+#    game.play(1, pieces['5I'], 0, 0, [3,3])
+#    for row in game.board:
+#        print row
+
+    
+#test()
---
+++ test/Main.py
@@ -0,0 +1,164 @@
+import wx
+import random
+from Board import *
+
+global tile_scale
+
+class Main(wx.Frame):
+    def __init__(self, parent, id, title):
+        wx.Frame.__init__(self, parent, id, title, size=(400, 400))
+
+        self.statusbar = self.CreateStatusBar()
+        self.statusbar.SetStatusText('0')
+        self.board = BoardContainer(self)
+        self.board.SetFocus()
+
+        self.Centre()
+        self.Show(True)
+        
+class BoardContainer(wx.Panel):
+    BoardWidth = 20
+    BoardHeight = 20
+#    Speed = 300
+    ID_TIMER = 1
+
+    def __init__(self, parent):
+        wx.Panel.__init__(self, parent, style=wx.WANTS_CHARS)
+
+        global tile_scale
+        tile_scale = 16
+
+#        self.timer = wx.Timer(self, Board.ID_TIMER)
+
+        self.game = Game()
+        game = self.game
+#        game.play(1, pieces['5W'], 1, 0, [0,0])
+#        game.play(2, pieces['5V'], 1, 0, [17,0])
+#        game.play(3, pieces['5V'], 2, 0, [17,17])
+#        game.play(4, pieces['5V'], 3, 0, [0,17])
+#        game.play(1, pieces['5I'], 0, 0, [3,3])
+        
+        self.curPiece = 0
+        self.curX = 0
+        self.curY = 0
+        self.curR = 0
+        self.curFlip = 0
+
+        self.Bind(wx.EVT_PAINT, self.OnPaint)
+        self.Bind(wx.EVT_KEY_DOWN, self.OnKeyDown)
+#        self.Bind(wx.EVT_TIMER, self.OnTimer, id=Board.ID_TIMER)
+        
+    def OnPaint(self, event):
+        dc = wx.PaintDC(self)
+        
+        game = self.game
+        
+        size = self.GetClientSize()
+        for i in range(len(game.board)):
+            for j in range(len(game.board)):
+#                shape = self.shapeAt(j, Board.BoardHeight - i - 1)
+#                if shape != Tetrominoes.NoShape:
+                    self.drawSquare(dc,
+                        0 + j * tile_scale,
+                        0 + i * tile_scale, game.board[j][i])
+
+#        if self.curPiece.shape() != Tetrominoes.NoShape:
+        p = pieces[pieces.keys()[self.curPiece]]
+        p = rotPiece(p, self.curR)
+        if self.curFlip:
+            p = flipPiece(p)
+        
+        for i in range(len(p)):
+            ix = self.curX + p[i][0]
+            iy = self.curY + p[i][1]
+            self.drawSquare(dc, 0 + ix * tile_scale,
+                0 + iy * tile_scale,
+                game.curr_turn)
+
+    def OnKeyDown(self, event):
+        keycode = event.GetKeyCode()
+
+#        if keycode == ord('P') or keycode == ord('p'):
+#            self.pause()
+#            return
+#        if self.isPaused:
+#            return
+        if keycode == wx.WXK_LEFT:
+            self.curX -= 1
+        elif keycode == wx.WXK_RIGHT:
+            self.curX += 1
+        elif keycode == wx.WXK_DOWN:
+            self.curY += 1
+        elif keycode == wx.WXK_UP:
+            self.curY -= 1
+        elif keycode == ord('Z'):
+            self.curR -= 1
+            self.curR &= 3
+        elif keycode == ord('X'):
+            self.curR += 1
+            self.curR &= 3
+        elif keycode == ord('C'):
+            self.curFlip ^= 1
+        elif keycode == ord('A'):
+            self.curPiece -= 1
+            self.curPiece %= len(pieces)
+        elif keycode == ord('S'):
+            self.curPiece += 1
+            self.curPiece %= len(pieces)
+        elif keycode == wx.WXK_SPACE:
+            success = self.game.play(self.game.curr_turn, pieces[pieces.keys()[self.curPiece]], self.curR, self.curFlip, [self.curX,self.curY])
+            if success:
+                self.game.curr_turn += 1
+                if self.game.curr_turn > 4:
+                    self.game.curr_turn = 1
+        else:
+            event.Skip()
+        
+        self.Refresh()
+#
+#
+#    def OnTimer(self, event):
+#        if event.GetId() == Board.ID_TIMER:
+#            if self.isWaitingAfterLine:
+#                self.isWaitingAfterLine = False
+#                self.newPiece()
+#            else:
+#                self.oneLineDown()
+#        else:
+#            event.Skip()
+
+    def drawSquare(self, dc, x, y, shape):
+        colors = ['#000000', '#CC6666', '#66CC66', '#6666CC',
+                  '#CCCC66', '#CC66CC', '#66CCCC', '#DAAA00']
+
+        light = ['#000000', '#F89FAB', '#79FC79', '#7979FC', 
+                 '#FCFC79', '#FC79FC', '#79FCFC', '#FCC600']
+
+        dark = ['#000000', '#803C3B', '#3B803B', '#3B3B80', 
+                 '#80803B', '#803B80', '#3B8080', '#806200']
+
+        pen = wx.Pen(light[shape])
+        pen.SetCap(wx.CAP_PROJECTING)
+        dc.SetPen(pen)
+
+        dc.DrawLine(x, y + tile_scale - 1, x, y)
+        dc.DrawLine(x, y, x + tile_scale - 1, y)
+
+        darkpen = wx.Pen(dark[shape])
+        darkpen.SetCap(wx.CAP_PROJECTING)
+        dc.SetPen(darkpen)
+
+        dc.DrawLine(x + 1, y + tile_scale - 1,
+            x + tile_scale - 1, y + tile_scale - 1)
+        dc.DrawLine(x + tile_scale - 1, 
+        y + tile_scale - 1, x + tile_scale - 1, y + 1)
+
+        dc.SetPen(wx.TRANSPARENT_PEN)
+        dc.SetBrush(wx.Brush(colors[shape]))
+        dc.DrawRectangle(x + 1, y + 1, tile_scale - 2, 
+        tile_scale - 2)
+
+
+app = wx.App(redirect = 0)
+Main(None, -1, 'Blokus')
+app.MainLoop()
